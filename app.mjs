d7fa096fee0c461676d925b26502fe29b527a882
Lukmauu <3298781+Lukmauu@users.noreply.github.com>
2025-02-22 12:14:49 -0800

    Try to commit with node_modules

--- app.mjs
+++ app.mjs
@@ -1,10 +1,14 @@
 import fs from 'fs';
 import moment from 'moment';
+import { SQSClient, ReceiveMessageCommand }from '@aws-sdk/client-sqs';
+
+const sqsClient = new SQSClient({region: 'us-east-1'});
+const queueURL = "https://sqs.us-east-1.amazonaws.com/000000000000/Project2Queue";
 
 // To start this fake service, simply run from the terminal:  npm start 
 // To stop the service:  Press Ctrl+C, and answer Yes
 
-const logFileName = 'Project2Service.log'
+const logFileName = 'Project2Service.log';
 
 // Use this function to write to the service log file.
 // Remember that you need to write to the log wvery time you:
@@ -24,6 +28,20 @@ function writeToLogFile(message) {
 async function readFromDownwardQueue() {
     // TODO:
     // 1- Check for messages in the DownwardQueue
+    const params = {
+        QueueURL: queueURL,
+        WaitTimeSeconds: 20,
+        MaxNumberOfMessages: 1
+    };
+
+    const command = new ReceiveMessageCommand(params);
+    const response = await sqsClient.send(command);
+
+    console.log(response);
+
+
+
+
     // 2- Query the database for insurance info (InsuranceDatabase.xml or InsuranceDatabase.json)
     // 3- Return data to UpwardQueue
 }
